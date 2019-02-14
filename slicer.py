7ef95ce2a5bc593e396ecdf0a519f359caaf13a5
Franciscus Pruis <fpruis@simplybillions.com>
2019-02-14 17:08:37 +0200

    User inputs a gmail, it will return your email is at Google.

--- slicer.py
+++ slicer.py
@@ -2,11 +2,14 @@
 email = input("What is your email address: ").strip()
 
 # Slice out user name
-user = email[ :email.index("@")]
+user = email[:email.index("@")]
 
 # Slice domain name
 domain = email[email.index("@") + 1:]
 
 # Format message
-output = "Your username is {} and your email is at {}".format(user,domain)
-print(output)
+if domain == "gmail.com":
+    gmail = "Google"
+    print("Your username is {} and all your emails are being tracked by {}.".format(user, gmail))
+else:
+    print("Your username is {} and your email address is at {}.".format(user, domain))
