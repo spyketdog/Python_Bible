b50c9c5ec57b78c9d4602f666a822102ef9905fa
Franciscus Pruis <fpruis@simplybillions.com>
2019-02-14 20:43:07 +0200

    New if_statement exercise.

---
+++ if.py
@@ -0,0 +1,9 @@
+num1 = 200
+num2 = 500
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num2 > num1:
+    print("num2 is bigger than num1")
+else:
+    print("both numbers are equal")
